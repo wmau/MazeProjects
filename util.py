b2ed20c99504b76620b93911b3066c5809146f9d
Will Mau <wmau@bu.edu>
2020-01-15 10:13:21 -0500

    Formatting changes

--- util.py
+++ util.py
@@ -2,16 +2,14 @@ from util import get_data_paths
 import os
 
 def make_pattern_dict():
-    pattern_dict = {}
-    pattern_dict['Arduino'] = 'H??_M??_S??.???? ????.txt'
-    pattern_dict['MiniscopeFolder'] = 'H??_M??_S??'
-    pattern_dict['BehaviorVideo'] = os.path.join(pattern_dict['MiniscopeFolder'],
-                                                 'Merged.avi')
-    pattern_dict['EZTrack'] = os.path.join(pattern_dict['MiniscopeFolder'],
-                                           '*_LocationOutput.csv')
-    pattern_dict['settings'] = os.path.join(pattern_dict['MiniscopeFolder'],
-                                            'settings_and_notes.dat')
-    pattern_dict['timestamps'] = os.path.join(pattern_dict['MiniscopeFolder'],
-                                              'timestamp.dat')
+    miniscope_folder = 'H??_M??_S??'
+    pattern_dict = {
+        'Arduino': 'H??_M??_S??.???? ????.txt',
+        'MiniscopeFolder': miniscope_folder,
+        'BehaviorVideo': os.path.join(miniscope_folder, 'Merged.avi'),
+        'EZTrack': os.path.join(miniscope_folder, '*_LocationOutput.csv'),
+        'settings': os.path.join(miniscope_folder, 'settings_and_notes.dat'),
+        'timestamps': os.path.join(miniscope_folder, 'timestamp.dat')
+    }
 
     return pattern_dict
