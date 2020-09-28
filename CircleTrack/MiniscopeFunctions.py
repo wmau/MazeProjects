bba87028bf8dd5364c4e67a1f504feb75f7dd0f0
Will Mau <wmau@bu.edu>
2020-09-28 18:34:24 -0400

    Can now plot spatial responses over trials

--- CircleTrack/MiniscopeFunctions.py
+++ CircleTrack/MiniscopeFunctions.py
@@ -1,12 +1,14 @@
 from CircleTrack.BehaviorFunctions import BehaviorSession, spiral_plot
 import matplotlib.pyplot as plt
 import numpy as np
-from CaImaging.util import sync_data
+from CaImaging.util import sync_data, nan_array
+from CaImaging.Miniscope import threshold_S
 from util import Session_Metadata
 from CircleTrack.BehaviorFunctions import linearize_trajectory
 from CaImaging.util import ScrollPlot
 from CircleTrack.plotting import plot_spiral
 from CaImaging.PlaceFields import PlaceFields
+from CaImaging.Behavior import spatial_bin
 
 class CalciumSession:
     def __init__(self, session_folder):
@@ -19,17 +21,19 @@ class CalciumSession:
         """
         # Get the behavioral data.
         self.folder = session_folder
-        self.BehaviorSession = BehaviorSession(self.folder)
+        self.data = {"behavior": BehaviorSession(self.folder)}
 
         # Get paths
-        self.minian_path = self.BehaviorSession.paths["minian"]
-        timestamp_paths = self.BehaviorSession.paths["timestamps"]
+        self.minian_path = self.data["behavior"].paths["minian"]
+        timestamp_paths = self.data["behavior"].paths["timestamps"]
 
         # Combine behavioral and calcium imaging data.
-        self.data = dict()
-        self.data["behavior"], self.data["imaging"], _ = sync_data(
-            self.BehaviorSession.behavior_df, self.minian_path, timestamp_paths
+        self.data["behavior"].behavior_df, self.data["imaging"], _ = sync_data(
+            self.data["behavior"].behavior_df, self.minian_path, timestamp_paths
         )
+
+        # Get number of neurons.
+        self.n_neurons = self.data['imaging']['C'].shape[0]
 
     def plot_spiral_spikes(self, first_neuron=0, S_thresh=1):
         """
@@ -49,10 +53,10 @@ class CalciumSession:
             activity > (np.mean(activity) + S_thresh * np.std(activity))
             for activity in self.data["imaging"]["S"]
         ]
-        t = np.asarray(self.data["behavior"].frame)
+        t = np.asarray(self.data["behavior"].behavior_df['frame'])
 
         # Linearize position.
-        lin_position = np.asarray(linearize_trajectory(self.data["behavior"])[0])
+        lin_position = np.asarray(linearize_trajectory(self.data["behavior"].behavior_df)[0])
 
         # Do the plot.
         cell_number_labels = [f"Cell #{n}" for n, _ in enumerate(spikes)]
@@ -68,17 +72,55 @@ class CalciumSession:
         )
 
 
-    def linearized_activity(self, bin_size=10):
+    def linearized_activity(self, bin_size=0.02):
         PFs = PlaceFields(np.asarray(self.data["behavior"]["lin_position"]),
                           np.zeros_like(self.data["behavior"]["lin_position"]),
                           self.data["imaging"]["S"],
-                          bin_size_cm=0.02,
+                          bin_size_cm=bin_size,
                           one_dim=True)
         pass
 
 
+    def spatial_activity_by_trial(self, bin_size_radians=0.02):
+        behavior_df = self.data["behavior"].behavior_df
+        lin_position = np.asarray(behavior_df["lin_position"])
+        filler = np.zeros_like(lin_position)
+
+        bin_edges = spatial_bin(lin_position, filler,
+                                bin_size_cm=bin_size_radians,
+                                one_dim=True)[1]
+
+        S = threshold_S(self.data["imaging"]["S"])
+
+        occ_map_by_trial = []
+        fields = nan_array((self.n_neurons,
+                            self.data["behavior"].ntrials,
+                            len(bin_edges)-1))
+        for trial_number in range(self.data["behavior"].ntrials):
+            time_bins = behavior_df['trials'] == trial_number
+            positions_this_trial = behavior_df.loc[time_bins, 'lin_position']
+            filler = np.zeros_like(positions_this_trial)
+            occupancy = spatial_bin(positions_this_trial,
+                                    filler,
+                                    bins=bin_edges,
+                                    one_dim=True)[0]
+
+            for n, neuron in enumerate(S):
+                spiking = neuron[time_bins]
+                fields[n, trial_number, :] = spatial_bin(positions_this_trial,
+                                                         filler,
+                                                         bins=bin_edges,
+                                                         one_dim=True,
+                                                         weights=spiking)[0]
+
+
+            occ_map_by_trial.append(occupancy)
+        occ_map_by_trial = np.vstack(occ_map_by_trial)
+
+        return fields, occ_map_by_trial
+
 if __name__ == "__main__":
     folder = r"Z:\Will\Drift\Data\Castor_Scope05\09_09_2020_CircleTrackGoals2\16_46_11"
     S = CalciumSession(folder)
-    S.linearized_activity()
+    S.spatial_activity_by_trial(0.1)
     pass
